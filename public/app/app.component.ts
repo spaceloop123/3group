9758a60649a7a8fb915eacc54b642340f8dad33d
Alexandra <alexandra.abramovich@gmail.com>
2016-07-08 20:44:46 +0300

    Added routing

--- public/app/app.component.ts
+++ public/app/app.component.ts
@@ -1,10 +1,38 @@
 import {Component} from "@angular/core";
-import {LoginFormComponent} from './login/login-form.component';
+import {LoginComponent} from './login/login.component';
+import {HomepageComponent} from './login/homepage.component';
+import {HeaderComponent} from './header/header.component';
+import {Page1Component} from './page1/page1.component';
+
+import {ROUTER_DIRECTIVES} from '@angular/router';
+import {LoginService} from "./login/login.service";
 
 @Component({
     selector: 'my-app',
-    templateUrl: './app/app.component.html',
-    directives: [LoginFormComponent]
+    template: `
+<div class="page-flexbox-wrapper">
+    <header>
+        <nav class="black navbar-fixed" role="navigation">
+            <div class="nav-wrapper container">
+               <header-component></header-component>
+           </div>
+       </nav>
+    </header>
+    <main class="valign-wrapper">
+        <router-outlet></router-outlet>
+    </main>
+    <footer class="page-footer" style="background-color: #000000">
+       <div class="footer-copyright">
+           <div class="container">
+               Copyright Text
+            </div>
+        </div>
+    </footer>
+</div>
+    `,
+    directives: [HeaderComponent, ROUTER_DIRECTIVES],
+    precompile: [LoginComponent, HomepageComponent, Page1Component],
+    providers: [LoginService]
 })
 
 export class AppComponent {
---
+++ public/app/header/header.component.ts
@@ -0,0 +1,16 @@
+/**
+ * Created by alexandra on 8.7.16.
+ */
+
+import { Component } from '@angular/core';
+import { ROUTER_DIRECTIVES } from '@angular/router';
+
+@Component({
+    selector: 'header-component',
+    templateUrl: 'app/header/header-logo.html',
+    directives: [ROUTER_DIRECTIVES]
+})
+
+export class HeaderComponent {
+
+}
