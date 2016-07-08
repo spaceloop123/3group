69ee942c983125a7fa6f25716d2dd0898631ae35
Alexandra <alexandra.abramovich@gmail.com>
2016-07-08 21:58:01 +0300

    Added routing final

--- public/app/main.ts
+++ public/app/main.ts
@@ -1,8 +1,7 @@
-import { bootstrap }    from '@angular/platform-browser-dynamic';
-import {Constants} from "../app/constants/constants.data";
-import { AppComponent } from './app.component';
+import {bootstrap} from "@angular/platform-browser-dynamic";
+import {AppComponent} from "./app.component";
 import {HTTP_PROVIDERS} from "@angular/http";
-import { APP_ROUTER_PROVIDERS } from './app.routes';
+import {APP_ROUTER_PROVIDERS} from "./app.routes";
 
 bootstrap(AppComponent, [
     HTTP_PROVIDERS, APP_ROUTER_PROVIDERS
