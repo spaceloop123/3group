c385e19aa814163cc755443f604cbcbb723209e4
j-and <evgenija.ostrovskaja@gmail.com>
2016-07-13 10:20:59 +0300

    Add files via upload
    added user.component.ts and user-home.html, changed homepage.html

---
+++ user.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+import {ROUTER_DIRECTIVES} from "@angular/router";
+
+@Component({
+    selector: 'user-component',
+    templateUrl: 'app/user/user-home.html',
+    directives: [ROUTER_DIRECTIVES]
+})
+
+export class UserComponent {
+
+}
