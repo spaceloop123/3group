b4307d5dbe09b4e64f14288ee29a06cb027a3dde
Alexandra <alexandra.abramovich@gmail.com>
2016-07-13 19:14:07 +0300

    Showing tests-to-check with *ngFor with random color

--- app/teacher/teacher.component.ts
+++ app/teacher/teacher.component.ts
@@ -1,48 +1,47 @@
 import {Component, OnInit} from '@angular/core';
 import {ROUTER_DIRECTIVES} from "@angular/router";
-import {CardsColorsData} from './cards-colors.data';
+import {TestsListData} from './tests-list.data';
+import {CardsColorsData} from "./cards-colors.data";
+import {Http} from "@angular/http";
 
 @Component({
     selector: 'teacher-component',
     templateUrl: 'app/teacher/teacher-home.html',
     directives: [ROUTER_DIRECTIVES],
-    providers: [CardsColorsData]
+    providers: [CardsColorsData, TestsListData]
 })
 
 export class TeacherComponent implements OnInit {
+    private assignedTests;
+    private color;
 
-    /*
-     constructor(private testsImg:TestsImgData) {}
-
-     private randomImg1 = this.testsImg.TESTS_IMG[Math.floor(Math.random()*this.testsImg.TESTS_IMG.length)];
-
-     //Without ng-repeat-----------------------------------------------------------------------------------
-
-     private randomImg2 = this.testsImg.TESTS_IMG[Math.floor(Math.random()*this.testsImg.TESTS_IMG.length)];
-     private randomImg3 = this.testsImg.TESTS_IMG[Math.floor(Math.random()*this.testsImg.TESTS_IMG.length)];
-     private randomImg4 = this.testsImg.TESTS_IMG[Math.floor(Math.random()*this.testsImg.TESTS_IMG.length)];
-     private randomImg5 = this.testsImg.TESTS_IMG[Math.floor(Math.random()*this.testsImg.TESTS_IMG.length)];
-     private randomImg6 = this.testsImg.TESTS_IMG[Math.floor(Math.random()*this.testsImg.TESTS_IMG.length)];
-     */
-
-    constructor(private cardsColorsData:CardsColorsData) {
+    constructor(private cardsColorsData:CardsColorsData,
+                private http:Http,
+                private testsListData:TestsListData) {
     }
 
     private generateRandomColor = function () {
+        //generates whole color name randomly
         this.randomColorLeft = this.cardsColorsData.CARDS_COLORS_LEFT[Math.floor(Math.random() * this.cardsColorsData.CARDS_COLORS_LEFT.length)];
         this.randomColorRight = this.cardsColorsData.CARDS_COLORS_RIGHT[Math.floor(Math.random() * this.cardsColorsData.CARDS_COLORS_RIGHT.length)];
         return (this.randomColorLeft + " " + this.randomColorRight);
     }
-    //Without ng-repeat-----------------------------------------------------------------------------------
 
-    private randomCol1 = this.generateRandomColor();
-    private randomCol2 = this.generateRandomColor();
-    private randomCol3 = this.generateRandomColor();
-    private randomCol4 = this.generateRandomColor();
-    private randomCol5 = this.generateRandomColor();
-    private randomCol6 = this.generateRandomColor();
+    getTests() {
+        this.assignedTests = this.testsListData.ASSIGNED_TESTS;
+        for (let i = 0; i < this.assignedTests.length; i++) {
+            this.assignedTests[i].color = this.generateRandomColor();
+        }
+        return (this.assignedTests);
+        //var that = this;
+        // this.http.get()
+        //     .toPromise()
+        //     .then(response => that.assignedTests = response.json().data)
+        //     .catch(this.handleError);
+    }
 
     ngOnInit() {
-        console.log(this.randomCol1);
+        this.getTests();
+        console.log(this.assignedTests);
     }
 }
---
+++ app/teacher/tests-list.data.ts
@@ -0,0 +1,33 @@
+export class TestsListData {
+    //constructor(public assignedTests:string) {
+    //}
+
+    public ASSIGNED_TESTS = [
+        {
+            name: 'Name1',
+            descr: 'Some pre-level1'
+        },
+        {
+            name: 'Name2',
+            descr: 'Some pre-level2'
+        },
+        {
+            name: 'Name3',
+            descr: 'Some pre-level3'
+        },
+        {
+            name: 'Name4',
+            descr: 'Some pre-level4'
+        },
+        {
+            name: 'Name5',
+            descr: 'Some pre-level5'
+        },
+        {
+            name: 'Name',
+            descr: 'Some pre-level6'
+        },
+
+    ]
+
+}
