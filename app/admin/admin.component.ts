e16b3b27a9f73ea8f1e5e8596d15a7fab2abbd36
Sanya <spaceloop123@outlook.com>
2016-07-13 17:57:49 +0300

    Added new-user request

--- app/admin/admin.component.ts
+++ app/admin/admin.component.ts
@@ -1,5 +1,6 @@
 import {Component} from "@angular/core";
 import {ROUTER_DIRECTIVES} from "@angular/router";
+import {Http, Headers} from "@angular/http";
 
 @Component({
     selector: 'admin-component',
@@ -8,6 +9,10 @@ import {ROUTER_DIRECTIVES} from "@angular/router";
 })
 
 export class AdminComponent {
+    constructor(private http:Http) {
+
+    }
+
     private member = {
         role: 'guest',
         username: '',
@@ -23,5 +28,12 @@ export class AdminComponent {
             this.member.username = '';
             this.member.password = '';
         }
+    }
+
+    addUser() {
+        var headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+        console.log(this.http.post("/user-" + this.member.role, JSON.stringify(this.member), {headers: headers})
+            .toPromise());
     }
 }
