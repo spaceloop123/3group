f8f1a08b36e71153f4827a4f3a101b7d46970376
j-and <evgenija.ostrovskaja@gmail.com>
2016-07-15 13:25:46 +0300

    no changes

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,12 +5,13 @@ import {HeaderComponent} from "./common/header/header.component";
 
 import {ROUTER_DIRECTIVES} from "@angular/router";
 import {LoginService} from "./login/login.service";
+import {UserComponent} from "./user/user.component";
 
 @Component({
     selector: 'my-app',
     templateUrl: 'app/app.component.html',
     directives: [HeaderComponent, ROUTER_DIRECTIVES],
-    precompile: [LoginComponent, HomepageComponent],
+    precompile: [LoginComponent, HomepageComponent, UserComponent],
     providers: [LoginService]
 })
 
