24e26e3f5f1a7627148e860f54cf32563fff3aa0
j-and <evgenija.ostrovskaja@gmail.com>
2016-07-22 09:57:29 +0300

    added chart

--- app/user/charts.component.ts
+++ app/user/charts.component.ts
@@ -12,58 +12,37 @@ import {CHART_DIRECTIVES} from 'ng2-charts/ng2-charts';
 })
 export class LineChartDemoComponent {
     // lineChart
-    public lineChartData:Array<any> = [
-        {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
-        {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'},
-        {data: [18, 48, 77, 9, 100, 27, 40], label: 'Series C'}
+       public lineChartData:Array<any> = [
+      //  {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
+        {data: [150,160,150], label: 'Series A'},
     ];
-    public lineChartLabels:Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+    //public lineChartLabels:Array<any> = ['Header', 'Result', 'Date'];
+    public lineChartLabels:Array<any> = ['20/04/2014', '20/04/2015', '20/04/2016'];
     public lineChartOptions:any = {
         animation: false,
-        responsive: true
+        responsive: true,
+        legend: {
+            display: false}
     };
     public lineChartColours:Array<any> = [
+
         { // grey
             backgroundColor: 'rgba(148,159,177,0.2)',
             borderColor: 'rgba(148,159,177,1)',
             pointBackgroundColor: 'rgba(148,159,177,1)',
             pointBorderColor: '#fff',
             pointHoverBackgroundColor: '#fff',
-            pointHoverBorderColor: 'rgba(148,159,177,0.8)'
-        },
-        { // dark grey
-            backgroundColor: 'rgba(77,83,96,0.2)',
-            borderColor: 'rgba(77,83,96,1)',
-            pointBackgroundColor: 'rgba(77,83,96,1)',
-            pointBorderColor: '#fff',
-            pointHoverBackgroundColor: '#fff',
-            pointHoverBorderColor: 'rgba(77,83,96,1)'
-        },
-        { // grey
-            backgroundColor: 'rgba(148,159,177,0.2)',
-            borderColor: 'rgba(148,159,177,1)',
-            pointBackgroundColor: 'rgba(148,159,177,1)',
-            pointBorderColor: '#fff',
-            pointHoverBackgroundColor: '#fff',
-            pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+            pointHoverBorderColor: 'rgba(148,159,177,0.8)',
+            tension:0,
+            fill: false,
+
+
         }
     ];
-    public lineChartLegend:boolean = true;
+
     public lineChartType:string = 'line';
 
-    public randomize():void {
-        let _lineChartData:Array<any> = new Array(this.lineChartData.length);
-        for (let i = 0; i < this.lineChartData.length; i++) {
-            _lineChartData[i] = {
-                data: new Array(this.lineChartData[i].data.length),
-                label: this.lineChartData[i].label
-            };
-            for (let j = 0; j < this.lineChartData[i].data.length; j++) {
-                _lineChartData[i].data[j] = Math.floor((Math.random() * 100) + 1);
-            }
-        }
-        this.lineChartData = _lineChartData;
-    }
+
 
     // events
     public chartClicked(e:any):void {
