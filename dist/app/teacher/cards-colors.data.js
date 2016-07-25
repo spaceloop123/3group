15a73e4b7e04b168cffb7d57968d730f3131e8e0
Alexandra <alexandra.abramovich@gmail.com>
2016-07-25 17:54:39 +0300

    bad carousel on teacher UI

---
+++ dist/app/teacher/cards-colors.data.js
@@ -0,0 +1,37 @@
+System.register([], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var CardsColorsData;
+    return {
+        setters:[],
+        execute: function() {
+            CardsColorsData = (function () {
+                function CardsColorsData() {
+                    this.CARDS_COLORS_ACCENT = [
+                        'blue',
+                        'purple darken-2',
+                        'red darken-4',
+                        'red darken-2',
+                        'red darken-1',
+                        'yellow accent-3',
+                        'teal darken-3',
+                        'teal lighten-1',
+                        'light-green',
+                        'amber darken-3'
+                    ];
+                    this.CARDS_COLORS_NEUTRAL = [
+                        'indigo',
+                        'blue-grey lighten-1',
+                        'indigo darken-3',
+                        'blue-grey lighten-4',
+                        'light-blue accent-2',
+                        'cyan lighten-4'
+                    ];
+                }
+                return CardsColorsData;
+            }());
+            exports_1("CardsColorsData", CardsColorsData);
+        }
+    }
+});
+//# sourceMappingURL=cards-colors.data.js.map
---
+++ dist/app/teacher/teacher.component.js
@@ -0,0 +1,90 @@
+System.register(["@angular/core", "@angular/router", "./cards-colors.data", "rxjs/add/operator/toPromise", "../common/services/CustomHttp", "angular2-materialize/dist/index"], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+        var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+        if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+        else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+        return c > 3 && r && Object.defineProperty(target, key, r), r;
+    };
+    var __metadata = (this && this.__metadata) || function (k, v) {
+        if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+    };
+    var core_1, router_1, cards_colors_data_1, CustomHttp_1, index_1;
+    var TeacherComponent;
+    return {
+        setters:[
+            function (core_1_1) {
+                core_1 = core_1_1;
+            },
+            function (router_1_1) {
+                router_1 = router_1_1;
+            },
+            function (cards_colors_data_1_1) {
+                cards_colors_data_1 = cards_colors_data_1_1;
+            },
+            function (_1) {},
+            function (CustomHttp_1_1) {
+                CustomHttp_1 = CustomHttp_1_1;
+            },
+            function (index_1_1) {
+                index_1 = index_1_1;
+            }],
+        execute: function() {
+            TeacherComponent = (function () {
+                function TeacherComponent(cardsColorsData, customHttp, router) {
+                    this.cardsColorsData = cardsColorsData;
+                    this.customHttp = customHttp;
+                    this.router = router;
+                    this.generateRandomColor = function () {
+                        //generates whole color name randomly
+                        this.randomColor = this.cardsColorsData.CARDS_COLORS_ACCENT[Math.floor(Math.random() * this.cardsColorsData.CARDS_COLORS_ACCENT.length)];
+                        return (this.randomColor);
+                    };
+                }
+                TeacherComponent.prototype.getTests = function () {
+                    var that = this;
+                    this.customHttp.get('/teacher/tests')
+                        .subscribe(function (response) { that.setTests(response.json()); });
+                    //.catch( that.handleError.bind(that));
+                };
+                TeacherComponent.prototype.setTests = function (response) {
+                    this.assignedTests = response;
+                    console.log(this.assignedTests);
+                    for (var i = 0; i < this.assignedTests.length; i++) {
+                        this.assignedTests[i].color = this.generateRandomColor();
+                    }
+                };
+                TeacherComponent.prototype.checkTest = function (test) {
+                    //this happens when teacher clicks CHECK button
+                    this.router.navigate(['/teacher/check_test', test.id]);
+                };
+                TeacherComponent.prototype.handleError = function (error) {
+                    return Promise.reject(error.message || error);
+                };
+                TeacherComponent.prototype.ngOnInit = function () {
+                    this.customHttp.checkRole();
+                    this.getTests();
+                    console.log(this.assignedTests);
+                };
+                TeacherComponent = __decorate([
+                    core_1.Component({
+                        selector: 'teacher-component',
+                        templateUrl: 'app/teacher/teacher-home.html',
+                        directives: [router_1.ROUTER_DIRECTIVES, index_1.MaterializeDirective],
+                        providers: [cards_colors_data_1.CardsColorsData, CustomHttp_1.CustomHttp]
+                    }), 
+                    __metadata('design:paramtypes', [cards_colors_data_1.CardsColorsData, CustomHttp_1.CustomHttp, router_1.Router])
+                ], TeacherComponent);
+                return TeacherComponent;
+            }());
+            exports_1("TeacherComponent", TeacherComponent);
+        }
+    }
+});
+/*
+ for (let i = 0; i < this.assignedTests.length; i++) {
+ this.assignedTests[i].color = this.generateRandomColor();
+ }
+ */ 
+//# sourceMappingURL=teacher.component.js.map
