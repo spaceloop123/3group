ed402b10acb134f856004314f2a2e190d939a7df
Sanya <spaceloop123@outlook.com>
2016-07-27 17:22:17 +0300

    Testing POST /add_questions

--- app/admin/actions/add-question/add-question.component.ts
+++ app/admin/actions/add-question/add-question.component.ts
@@ -6,6 +6,7 @@ import {TestQuestionComponent} from "./question-type/test/test-question.componen
 import {OpenInsertQuestionComponent} from "./question-type/open-insert/open-insert-question.component";
 import {TestQuestion} from "./question-type/test/test-question.class";
 import {OpenInsertQuestion} from "./question-type/open-insert/open-insert-question.class";
+import {Headers, Http} from "@angular/http";
 
 @Component({
     selector: 'add-question-component',
@@ -26,7 +27,7 @@ export class AddQuestionComponent implements OnInit {
         this.selectedQuestion = this.questionsCatalog[0].type;
     }
 
-    constructor() {
+    constructor(private http:Http) {
         this.questionsList = [];
         this.questionsCatalog = [{type: new TestQuestion().type, checked: true},
             {type: new OpenInsertQuestion().type, checked: false}];
@@ -66,6 +67,13 @@ export class AddQuestionComponent implements OnInit {
             return;
         }
         console.log(JSON.stringify(this.questionsList));
+
+        var headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+        this.http
+            .post("/add_questions", JSON.stringify(this.questionsList), {headers: headers})
+            .toPromise()
+            .then(response => console.log(response.json()));
     }
 
     onQuestionCreate(responce, idx):void {
