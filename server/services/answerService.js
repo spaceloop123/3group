14abf645502faf7c158661334a2690b90dd6fd1b
AnyKey <mr.anikeev.97@gmail.com>
2016-07-27 14:49:59 +0300

    Added notification's model Update answerService & testService Update teacher's routes

--- server/services/answerService.js
+++ server/services/answerService.js
@@ -1,2 +1,15 @@
 var mongoose = require('mongoose');
 var Answer = mongoose.model('Answer');
+var Validator = require('../libs/requestValidator');
+
+module.exports.getAnswerById = function (answerId, done) {
+    var validator = new Validator();
+    
+    validator.checkItem('answer', function (callback) {
+        Answer.findOne({_id: answerId}).populate('question').exec(callback);
+    });
+    
+    validator.exec(function (res) {
+        done(null, res.answer.getAnswer());
+    }, done, done);
+};
