26355aa8df0044fde35aa7fe5385353d81a7bd98
alexandra1804 <alexandra.abramovich@gmail.com>
2016-08-02 17:42:19 +0300

    assign test fix

--- app/admin/actions/show-users/user-info/assignTest.component.ts
+++ app/admin/actions/show-users/user-info/assignTest.component.ts
@@ -5,19 +5,22 @@ import {CHART_DIRECTIVES} from "ng2-charts/ng2-charts";
 import {MaterializeDirective} from "angular2-materialize";
 import {ChartsComponent} from "../../../../user/charts/charts.component";
 import {CustomHttp} from "../../../../common/services/CustomHttp";
+import {InfiniteScroll} from "angular2-infinite-scroll/angular2-infinite-scroll";
 
 
 @Component({
 
     templateUrl: 'app/admin/actions/show-users/user-info/assignTest.html',
-    directives: [DatepickerComponent, ChartsComponent, CHART_DIRECTIVES, ROUTER_DIRECTIVES, MaterializeDirective],
+    directives: [DatepickerComponent, ChartsComponent, CHART_DIRECTIVES, ROUTER_DIRECTIVES, MaterializeDirective, InfiniteScroll],
 
 })
 
 export class AssignTestComponent implements OnInit {
 
     public currentUser:any;
+    public assignedTeacher:any;
     private sub;
+    teacherList = [];
 
     onNotify(message:string):void {
         var field = <HTMLElement><any>document.getElementById("datepicker");
@@ -29,10 +32,43 @@ export class AssignTestComponent implements OnInit {
         }
     }
 
+    assignTeacher(teacher) {
+        this.assignedTeacher = teacher;
+        console.log(this.assignedTeacher);
+    }
+
+    getTeacherList() {
+        var that = this;
+        this.customHttp.get('/admin/teachers_list')
+            .subscribe(response => {
+                console.log('posted');
+                that.setTeacherList(response.json());
+            });
+    }
+
+    setTeacherList(response) {
+        this.teacherList = this.teacherList.concat(response);
+        console.log(this.teacherList);
+    }
+
+    // onScrollDown () {
+    //     console.log('scrolled down!!');
+    //     this.shownUsers += 10;
+    //     this.getUsers();
+    // }
+    //
+    // scrollOrNot() {
+    //     if(this.scrollCount <= 10) {
+    //         console.log(this.scrollCount);
+    //         this.scrollCount++;
+    //     } else {
+    //         this.onScrollDown();
+    //         this.scrollCount = 0;
+    //     }
+    // }
 
     checkInput(event) {
         console.log("checkInput")
-
     }
 
 
@@ -41,8 +77,9 @@ export class AssignTestComponent implements OnInit {
     }
 
     ngOnInit() {
-        console.log('hui');
+        //TODO check test status for user and block test assignment if test is requested or has been assigned
         this.customHttp.checkRole();
+        this.getTeacherList();
         var that = this;
         this.sub = this.route.params.subscribe(params => {
             that.currentUser = params['id'];
