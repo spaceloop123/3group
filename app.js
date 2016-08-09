69ed629c452155a5a4723895f22e54b18aede681
alexandra1804 <alexandra.abramovich@gmail.com>
2016-08-10 00:10:42 +0300

    added using favicon to app.js

--- app.js
+++ app.js
@@ -56,8 +56,7 @@ app.set('views', path.join(__dirname, 'app'));
 app.set('view engine', 'ejs');
 
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(__dirname + '/app/assets/images/favicon.ico'));
 
 require('./server/config/passport');
 
